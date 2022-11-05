a5f7cafd1ce6a4051ce0c6726c8b0582fe615718
billzhangsc <billzhangsc@gmail.com>
2022-11-05 16:43:37 -0700

    Router

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import './App.css';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import {Landing} from './pages/Landing';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </div>
   );
 }
