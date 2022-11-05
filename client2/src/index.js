37e35d26105c1ef9f48253d9352d57de79efbb16
billzhangsc <billzhangsc@gmail.com>
2022-11-05 16:17:06 -0700

    Starter browser router

--- client2/src/index.js
+++ client2/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App.js';
+import {BrowserRouter} from 'react-router-dom';
 import Game from './Game';
 // import PongComponent from './Pong';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<PongComponent />);
-root.render(<Game />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
