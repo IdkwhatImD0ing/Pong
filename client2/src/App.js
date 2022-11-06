80614faedfde99539d4dd16d84c8961ffd8f80dd
billzhangsc <billzhangsc@gmail.com>
2022-11-05 17:04:01 -0700

    Linking to local

--- client2/src/App.js
+++ client2/src/App.js
@@ -2,12 +2,14 @@ import * as React from 'react';
 import './App.css';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 import {Landing} from './pages/Landing';
+import Game from './Game';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </div>
   );
--- client2/src/pages/Landing.js
+++ client2/src/pages/Landing.js
@@ -1,9 +1,9 @@
-import * as React from 'react'
-import * as MuiComponents from '../components/MuiComponents.js'
-import { Box } from '@mui/material'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import * as React from 'react';
+import * as MuiComponents from '../components/MuiComponents.js';
+import {Box} from '@mui/material';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Stack } from '@mui/system';
+import {Stack} from '@mui/system';
 
 const darkTheme = createTheme({
   palette: {
@@ -17,6 +17,7 @@ const RoomContext = React.createContext();
 export function Landing() {
   const [name, setName] = React.useState(null);
   const [room, setRoom] = React.useState(null);
+
   return (
     <div>
       <ThemeProvider theme={darkTheme}>
@@ -24,18 +25,19 @@ export function Landing() {
         <UserContext.Provider value={{name, setName}}>
           <MuiComponents.PongAppBar />
           <Box height={'10vh'} />
-          {name === null ? (<MuiComponents.WelcomePrompt/>) :
-          (
-            <Stack direction='column' spacing={3}>
+          {name === null ? (
+            <MuiComponents.WelcomePrompt />
+          ) : (
+            <Stack direction="column" spacing={3}>
               <RoomContext.Provider value={{room, setRoom}}>
                 <MuiComponents.JoinPrompt />
               </RoomContext.Provider>
 
-              <Box sx={{ display:'flex', justifyContent:'space-between'}}>
-                <div/>
+              <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <div />
                 <MuiComponents.CardPlayLocal />
                 <MuiComponents.CardPlayOnline />
-                <div/>
+                <div />
               </Box>
             </Stack>
           )}
